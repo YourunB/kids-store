6e30470d200edf88ee063e6372319ac3ad067cee
yourunb <bxyura@gmail.com>
2024-11-14 15:29:56 +0300

    feat: add slider for many cards

---
+++ scripts/swiper/five-swiper.js
@@ -0,0 +1,7 @@
+const swiperFive = new Swiper('.swiper-container', {
+  slidesPerView: 5, // Показываем 5 слайдов
+  spaceBetween: 10, // Расстояние между слайдами
+  freeMode: true,   // Без циклической прокрутки
+  loop: false,      // Без бесконечной прокрутки
+  grabCursor: true, // Курсор захвата для мобильных устройств
+});
