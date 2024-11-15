9153b6db7a4e7fc9fd1022ce38f18509d92f8105
yourunb <bxyura@gmail.com>
2024-11-15 17:37:18 +0300

    feat: change logo image on scroll page

--- scripts/menu-scroll-main-page.js
+++ scripts/menu-scroll-main-page.js
@@ -1,5 +1,6 @@
 const header = document.querySelector('.header');
-
+const logo = document.querySelector('#header-logo');
+console.log(logo)
 let timeoutHeader;
 
 window.addEventListener('scroll', () => {
@@ -9,10 +10,12 @@ window.addEventListener('scroll', () => {
       header.classList.add('header_scrolled');
       header.classList.add('header-def');
       header.classList.remove('header-main');
+      logo.src = 'assets/images/svg/logo.svg';
     } else {
       header.classList.remove('header_scrolled');
       header.classList.remove('header-def');
       header.classList.add('header-main');
+      logo.src = 'assets/images/svg/logo-white.svg';
     }
   }, 250);
 });
