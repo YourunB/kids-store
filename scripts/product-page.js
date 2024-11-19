d382577ff2f108a5174f882481d150cc81d31662
yourunb <bxyura@gmail.com>
2024-11-19 22:29:18 +0300

    feat: add close / open spec info

--- scripts/product-page.js
+++ scripts/product-page.js
@@ -1,5 +1,7 @@
 const btnOpenCloseSpec = document.getElementById('btn-show-hide-spec');
+const contentDescription = document.getElementById('description-content');
 
 btnOpenCloseSpec.addEventListener('click', () => {
   btnOpenCloseSpec.classList.toggle('spec-content__show-hide_open');
+  contentDescription.classList.toggle('spec-content__description_open');
 });
