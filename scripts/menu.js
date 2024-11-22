bba2cbfc56d629cd32c73b242e347158961e735b
yourunb <bxyura@gmail.com>
2024-11-22 12:01:16 +0300

    feat: add icon rotate

--- scripts/menu.js
+++ scripts/menu.js
@@ -17,11 +17,14 @@ btnSearch.addEventListener('click', () => {
 
 const btnToggleLang = document.getElementById('btn-toggle-lang');
 const windowLang = document.getElementById('window-lang');
+const btnLangArrow = document.getElementById('btn-toggle-lang-img');
 
 btnToggleLang.addEventListener('click', () => {
   windowLang.classList.toggle('menu-language__window_open');
+  btnLangArrow.classList.toggle('menu-icon_rotate');
 });
 
 windowLang.addEventListener('click', (e) => {
   if (e.target.tagName === 'SPAN') windowLang.classList.remove('menu-language__window_open');
+  btnLangArrow.classList.remove('menu-icon_rotate');
 });
