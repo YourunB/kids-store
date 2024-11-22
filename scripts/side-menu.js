ad2f0a886b3aa7a304a6600ca9b1b62df7101d7d
yourunb <bxyura@gmail.com>
2024-11-22 19:15:33 +0300

    feat: add btns for filters and catalog

--- scripts/side-menu.js
+++ scripts/side-menu.js
@@ -5,3 +5,16 @@ sideMenu.addEventListener('click', (e) => {
     e.target.classList.toggle('side-menu__item_open');
   }
 })
+
+const btnCatalog = document.getElementById('btn-catalog');
+const btnFilters = document.getElementById('btn-filters');
+const catalog = document.getElementById('side-menu');
+const filters = document.getElementById('filters-block');
+
+btnCatalog.addEventListener('click', () => {
+  catalog.classList.toggle('side-menu_open');
+});
+
+btnFilters.addEventListener('click', () => {
+  filters.classList.toggle('sort-top_open');
+});
