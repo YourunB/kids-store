a0cc8615102a618a2c752a03d7aa478959f1bac6
yourunb <bxyura@gmail.com>
2024-11-22 11:58:39 +0300

    feat: add lang menu window

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,4 +1,6 @@
 
+/*-------------------------------windows-search--------------------------*/
+
 const btnToggleSearch = document.getElementById('btn-toggle-search');
 const windowSearch = document.getElementById('window-search');
 const btnSearch = document.getElementById('btn-search');
@@ -9,4 +11,17 @@ btnToggleSearch.addEventListener('click', () => {
 
 btnSearch.addEventListener('click', () => {
   windowSearch.classList.toggle('menu-search__window_open');
+});
+
+/*-------------------------------windows-language--------------------------*/
+
+const btnToggleLang = document.getElementById('btn-toggle-lang');
+const windowLang = document.getElementById('window-lang');
+
+btnToggleLang.addEventListener('click', () => {
+  windowLang.classList.toggle('menu-language__window_open');
+});
+
+windowLang.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') windowLang.classList.remove('menu-language__window_open');
 });
